816cc67e91fdbe1aa6761125c439f1bbfefee711
mr-mixas <mixas.sr@gmail.com>
2016-02-07 14:46:58 +0300

    Data::Diff added to comparsion (loads if available)

--- benchmark/benchmark.pl
+++ benchmark/benchmark.pl
@@ -12,9 +12,14 @@ use Structs;
 
 my $cloned = dclone($Structs::STRUCT1);
 
+my $DD;
+eval "use Data::Diff qw(Diff)";
+$DD = 1 unless ($@);
+
 for my $type ('AoA', 'HoH', 'MIX') {
     my $rivals = {};
     $rivals->{"SD_${type}_det0"} = sub { diff($Structs::STRUCT1->{"${type}"}, $cloned->{"${type}"}), 'detailed' => 0 };
     $rivals->{"SD_${type}_det1"} = sub { diff($Structs::STRUCT1->{"${type}"}, $cloned->{"${type}"}), 'detailed' => 1 };
-    cmpthese (100, $rivals);
+    $rivals->{"DD_${type}"} = sub { Diff($Structs::STRUCT1->{"${type}"}, $cloned->{"${type}"}) } if ($DD);
+    cmpthese (50, $rivals);
 }
