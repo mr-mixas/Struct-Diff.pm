4c84cda05ec500ad2d14b5a5281f85ed956b11ce
mr-mixas <mixas.sr@gmail.com>
2016-04-05 22:36:33 +0300

    Data::Compare to competition added

--- benchmark/benchmark.pl
+++ benchmark/benchmark.pl
@@ -12,13 +12,17 @@ use Structs;
 
 my $cloned = dclone($Structs::STRUCT1);
 
-my $DD;
+my ($DD, $DC);
 eval "use Data::Diff qw(Diff)";
 $DD = 1 unless ($@);
+
+eval "use Data::Compare";
+$DC = 1 unless ($@);
 
 for my $type ('AoA', 'HoH', 'MIX') {
     my $rivals = {};
     $rivals->{"SD_${type}"} = sub { diff($Structs::STRUCT1->{"${type}"}, $cloned->{"${type}"}) };
     $rivals->{"DD_${type}"} = sub { Diff($Structs::STRUCT1->{"${type}"}, $cloned->{"${type}"}) } if ($DD);
+    $rivals->{"DC_${type}"} = sub { Compare($Structs::STRUCT1->{"${type}"}, $cloned->{"${type}"}) } if ($DC);
     cmpthese (50, $rivals);
 }
