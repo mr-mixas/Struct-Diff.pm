10f106b03c79cb51bc1b3a16e8a7185a60b3ae37
mr-mixas <mixas.sr@gmail.com>
2016-07-02 00:05:03 +0300

    Deepcopy opt enabled

--- t/_common.pm
+++ t/_common.pm
@@ -8,8 +8,8 @@ use parent qw(Exporter);
 our @EXPORT_OK = qw(scmp);
 
 sub scmp($$$) { # compare structures by data
-    my $got = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Dump();
-    my $exp = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Dump();
+    my $got = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
+    my $exp = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
     print STDERR "\nDEBUG: === " . shift . " ===\ngot: $got\nexp: $exp\n" if ($ENV{DEBUG});
     return $got eq $exp;
 }
