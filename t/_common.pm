69ae36d5ef82f6e7f0bcf7214f361b9d229af71a
mr-mixas <mixas.sr@gmail.com>
2016-07-08 21:46:33 +0300

    refactoring, sdump() appeared

--- t/_common.pm
+++ t/_common.pm
@@ -2,14 +2,18 @@ package _common;
 
 # common parts for Struct::Path tests
 
-use Data::Dumper;
+use Data::Dumper qw();
 use parent qw(Exporter);
 
-our @EXPORT_OK = qw(scmp);
+our @EXPORT_OK = qw(scmp sdump);
+
+sub sdump($) {
+    return Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
+}
 
 sub scmp($$$) { # compare structures by data
-    my $got = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
-    my $exp = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
+    my $got = sdump(shift);
+    my $exp = sdump(shift);
     print STDERR "\nDEBUG: === " . shift . " ===\ngot: $got\nexp: $exp\n" if ($ENV{DEBUG});
     return $got eq $exp;
 }
